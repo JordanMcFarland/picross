45075c77445c31cd294fcfa9809e6d512e4fa05e
Jordan McFarland <jdmcfarland4591@gmail.com>
2022-09-14 01:27:03 -0400

    Restructured grid state, added ability to drag, get new puzzle

--- src/components/Picross.js
+++ src/components/Picross.js
@@ -2,41 +2,41 @@ import React, { useEffect, useState } from "react";
 import PUZZLES from "../shared/PUZZLES";
 
 const PicrossGrid = ({ setSolved }) => {
-  const [grid, setGrid] = useState([["x", ""]]);
-  const [correctCoords, setCorrectCoords] = useState({});
-  const [userCoordinates, setUserCoordinates] = useState({});
+  const [grid, setGrid] = useState([[]]);
   const [rowColCounts, setRowColCounts] = useState({
     row: [],
     col: [],
   });
   const [currentTool, setCurrentTool] = useState("fillInTool");
+  const [dragging, setDragging] = useState(false);
+  const [dragState, setDragState] = useState();
+  const [newGridSize, setNewGridSize] = useState("fiveByFive");
 
-  // Get the correct coordinates of a new puzzle
+  // Listen for mouse up anywhere on screen so drag state updates properly
   useEffect(() => {
-    function getCorrectCoords() {
-      const tempObj = {};
-      grid.forEach((row, yIndex) => {
-        if (!tempObj[yIndex]) tempObj[yIndex] = [];
-        row.forEach((col, xIndex) => {
-          col === "x" && tempObj[yIndex].push(xIndex);
-        });
-      });
-      setCorrectCoords(tempObj);
+    function handleMouseUp() {
+      setDragging(false);
     }
-    getCorrectCoords();
-  }, [grid]);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => window.removeEventListener("mouseup", handleMouseUp);
+  }, []);
+
+  // Get initial puzzle
+  useEffect(() => {
+    getPuzzle("fiveByFive");
+  }, []);
 
   // Update the puzzle column and row counts when a new puzzle comes up
   useEffect(() => {
-    console.log(grid);
     function getRowCounts() {
       let rowCountArr = [];
 
-      grid.forEach((row, yIndex) => {
+      grid?.forEach((row, yIndex) => {
         const currentRowArr = [];
         let counter = 0;
         row.forEach((col, xIndex) => {
-          if (col === "x") {
+          if (col.marked) {
             counter++;
           } else {
             counter > 0 && currentRowArr.push(counter);
@@ -55,8 +55,6 @@ const PicrossGrid = ({ setSolved }) => {
         });
         rowCountArr.push(currentRowArr);
       });
-      console.log(rowCountArr);
-
       return rowCountArr;
     }
 
@@ -67,7 +65,7 @@ const PicrossGrid = ({ setSolved }) => {
         let counter = 0;
         const currentColCount = [];
         grid.forEach((row, yIndex) => {
-          if (row[xIndex] === "x") {
+          if (row[xIndex].marked) {
             counter++;
           } else {
             counter > 0 && currentColCount.push(counter);
@@ -93,52 +91,62 @@ const PicrossGrid = ({ setSolved }) => {
     setRowColCounts({ row: getRowCounts(), col: getColCounts() });
   }, [grid]);
 
-  // Check if puzzle is correct when user changes an input
+  // Check if puzzle is correct when user changes a grid square state
   useEffect(() => {
     function checkIfPicrossIsCorrect() {
-      const keys = Object.keys(correctCoords);
-
-      const solved = keys.every((key) => {
-        if (userCoordinates[key]) {
-          return (
-            correctCoords[key].every((xValue) =>
-              userCoordinates[key].includes(xValue)
-            ) && correctCoords[key].length === userCoordinates[key].length
-          );
-        }
-        return false;
+      const puzzleSolved = grid.every((row) => {
+        return row.every((col) => {
+          if (col.marked && col.state === "active") return true;
+          else if (!col.marked && col.state !== "active") return true;
+          else return false;
+        });
       });
 
-      solved && setSolved(true);
+      setSolved(puzzleSolved);
     }
 
-    if (Object.keys(userCoordinates).length) {
-      checkIfPicrossIsCorrect();
-    }
-  }, [userCoordinates]);
+    checkIfPicrossIsCorrect();
+  }, [grid]);
 
   function getPuzzle(puzzleSize) {
     let newPuzzle = [];
     const keys = Object.keys(PUZZLES[puzzleSize]);
     const rand = Math.floor(Math.random() * keys.length);
     const newPuzzleName = keys[rand];
-    console.log(PUZZLES[puzzleSize][newPuzzleName]);
-    newPuzzle = PUZZLES[puzzleSize][newPuzzleName];
+    newPuzzle = setANewGrid(PUZZLES[puzzleSize][newPuzzleName]);
     setSolved(false);
-    setUserCoordinates({});
     setGrid(newPuzzle);
+  }
+
+  function setANewGrid(newPuzzle) {
+    let newGrid = [];
+    newPuzzle.forEach((row) => {
+      const currentRow = [];
+      row.forEach((xIndex) => {
+        const xIndexObj = { marked: false, state: "inactive" };
+        if (xIndex === "x") {
+          xIndexObj.marked = true;
+        }
+        currentRow.push(xIndexObj);
+      });
+      newGrid.push(currentRow);
+    });
+    return newGrid;
   }
 
   function renderColumnNumberRow() {
     return (
       <div className="gridRow">
-        <div style={{ width: 40 }}></div>
         {rowColCounts.col.map((colArr, index) => {
           return (
             <div key={index} className="verticalNumContainer">
               {colArr.map((num, numIndex) => {
                 return (
-                  <div key={numIndex} className="yNumCount">
+                  <div
+                    key={numIndex}
+                    className="yNumCount"
+                    style={{ userSelect: "none", fontWeight: "bold" }}
+                  >
                     {num}
                   </div>
                 );
@@ -152,11 +160,11 @@ const PicrossGrid = ({ setSolved }) => {
 
   function renderRowNumbers(yIndex) {
     return (
-      <div className="horizontalNumContainer">
+      <div key={"row" + yIndex} className="horizontalNumContainer">
         {rowColCounts.row[yIndex]?.map((num, index) => {
           return (
             <div key={index} className="xNumCount">
-              <p>{num}</p>
+              <p style={{ userSelect: "none", fontWeight: "bold" }}>{num}</p>
             </div>
           );
         })}
@@ -168,16 +176,23 @@ const PicrossGrid = ({ setSolved }) => {
     return (
       <>
         {yIndex === 0 && renderColumnNumberRow()}
-        <div className="gridRow">
+        <div key={yIndex} className="gridRow">
           {rowColCounts.row.length && renderRowNumbers(yIndex)}
           {row.map((col, xIndex) => {
             return (
               <GridSquare
                 key={xIndex}
+                x={xIndex}
+                y={yIndex}
+                state={col.state}
                 coordinates={{ x: xIndex, y: yIndex }}
-                setUserCoordinates={setUserCoordinates}
-                userCoordinates={userCoordinates}
                 currentTool={currentTool}
+                dragging={dragging}
+                setDragging={setDragging}
+                dragState={dragState}
+                setDragState={setDragState}
+                grid={grid}
+                setGrid={setGrid}
               />
             );
           })}
@@ -189,8 +204,30 @@ const PicrossGrid = ({ setSolved }) => {
   return (
     <>
       {renderGrid}
-      <button onClick={() => getPuzzle("fiveByFive")}>Test</button>
-      <button onClick={() => console.log(correctCoords)}>Grid</button>
+      <div
+        style={{
+          marginTop: 16,
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <button onClick={() => getPuzzle(newGridSize)}>New Puzzle</button>
+        <label
+          htmlFor="newPuzzleSize"
+          style={{ display: "flex", alignItems: "center", marginRight: 4 }}
+        >
+          Size:{" "}
+        </label>
+        <select
+          defaultValue="fiveByFive"
+          name="newPuzzleSize"
+          id="newPuzzleSize"
+          onChange={(e) => setNewGridSize(e.target.value)}
+        >
+          <option value="fiveByFive">5x5</option>
+          <option value="tenByTen">10x10</option>
+        </select>
+      </div>
       <ToolSelector currentTool={currentTool} setCurrentTool={setCurrentTool} />
     </>
   );
@@ -198,45 +235,51 @@ const PicrossGrid = ({ setSolved }) => {
 
 const GridSquare = ({
   coordinates,
-  userCoordinates,
-  setUserCoordinates,
   currentTool,
+  dragging,
+  setDragging,
+  dragState,
+  setDragState,
+  grid,
+  setGrid,
+  state,
 }) => {
-  const [state, setState] = useState("inactive");
-
-  // Update User Inputs
-  useEffect(() => {
-    function updateUserCoordinates() {
-      const tempObj = { ...userCoordinates };
-      if (!tempObj[coordinates.y]) tempObj[coordinates.y] = [];
-      if (state === "active") {
-        if (!tempObj[coordinates.y].includes[coordinates.x]) {
-          tempObj[coordinates.y].push(coordinates.x);
-          tempObj[coordinates.y].sort();
-        }
-      } else {
-        const filteredCoords = tempObj[coordinates.y].filter(
-          (xValue) => xValue !== coordinates.x
-        );
-        tempObj[coordinates.y] = filteredCoords;
-      }
-      setUserCoordinates(tempObj);
-    }
-
-    updateUserCoordinates();
-  }, [state]);
-
-  function toggleGridSquare() {
+  function getDragState() {
     switch (currentTool) {
       case "fillInTool":
-        setState(state === "active" ? "inactive" : "active");
+        setDragState(state !== "active" ? "active" : "inactive");
         break;
       case "xTool":
-        setState(state === "dead" ? "inactive" : "dead");
+        setDragState(state !== "dead" ? "dead" : "inactive");
+    }
+  }
+
+  // update the state of the toggled grid square
+  function ToggleGridSquare() {
+    const newGrid = [...grid];
+    let toState;
+
+    switch (currentTool) {
+      case "fillInTool":
+        dragging
+          ? (toState = dragState)
+          : (toState = state === "active" ? "inactive" : "active");
+        break;
+      case "xTool":
+        dragging
+          ? (toState = dragState)
+          : (toState = state === "dead" ? "inactive" : "dead");
         break;
       default:
+        toState = "inactive";
         break;
     }
+
+    newGrid[coordinates.y][coordinates.x] = {
+      ...newGrid[coordinates.y][coordinates.x],
+      state: toState,
+    };
+    setGrid(newGrid);
   }
 
   return (
@@ -244,13 +287,14 @@ const GridSquare = ({
       className="gridSquare"
       style={{
         backgroundColor: state === "active" ? "black" : "white",
-        borderColor: state === "active" ? "grey" : "black",
-        height: 38,
-        width: 38,
+        outlineColor: state === "active" ? "grey" : "black",
       }}
-      onClick={() => {
-        toggleGridSquare();
+      onMouseDown={() => {
+        ToggleGridSquare();
+        getDragState();
+        setDragging(true);
       }}
+      onMouseOver={() => dragging && ToggleGridSquare()}
     >
       {state === "dead" && <p style={{ userSelect: "none" }}>X</p>}
     </div>
@@ -274,12 +318,12 @@ const ToolSelector = ({ currentTool, setCurrentTool }) => {
       <div
         className="fillInTool"
         onClick={() => setCurrentTool("fillInTool")}
-        style={{ borderWidth: currentTool === "fillInTool" && 5 }}
+        style={{ outlineWidth: currentTool === "fillInTool" && 5 }}
       />
       <div
         className="xTool"
         onClick={() => setCurrentTool("xTool")}
-        style={{ borderWidth: currentTool === "xTool" && 5 }}
+        style={{ outlineWidth: currentTool === "xTool" && 5 }}
       >
         <p style={{ userSelect: "none" }}>X</p>
       </div>
